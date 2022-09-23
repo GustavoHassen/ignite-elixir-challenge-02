5ad5380b655e0ae142089c8696a136fe9a7e3543
Gustavo Hassen <gustavo.hassen91@gmail.com>
2022-09-23 00:05:05 -0300

    Solution of the problem and test

---
+++ test/list_filter_test.exs
@@ -0,0 +1,11 @@
+defmodule ListFilterTest do
+  use ExUnit.Case
+
+  test "should return a odd numbers" do
+    list = ["1", "3", "6", "43", "banana", "6", "abc"]
+    expect_response = 3
+    response = ListFilter.call(list)
+
+    assert response == expect_response
+  end
+end
